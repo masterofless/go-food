1fe9c31fa92491f74b23f14b7746df85a0dfa339
Andy Cohan <cohana@avalonconsult.com>
2020-02-04 18:37:04 -0500

    started more http stuff

--- containers/food-api/cmd/food-api/main.go
+++ containers/food-api/cmd/food-api/main.go
@@ -23,18 +23,38 @@ type Foodstuff struct {
 	AKA           string             `json:"aka,omitempty" bson:"aka,omitempty"`
 }
 
-func CreateFoodstuffEndpoint(response http.ResponseWriter, request *http.Request) {}
-func GetRecipesEndpoint(response http.ResponseWriter, request *http.Request) { }
-func GetFoodstuffEndpoint(response http.ResponseWriter, request *http.Request) { }
+func dump(r *http.Request) {
+	output, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		fmt.Println("Error dumping request:", err)
+		return
+	}
+	fmt.Println(string(output))
+}
+
+func CreateFoodstuffEndpoint(response http.ResponseWriter, request *http.Request) {
+	dump(request)
+	newFoodstuff := Foodstuff{"", "Brussel Sprouts", "roasted yumminess", []string{"leafy veggie balls"}}
+	dbContext, err := GetDbContext("")
+	fmt.Fprintf(os.Stdout, "Inserting Foodstuff %v", newFoodstuff)
+	result, e2 := dbContext.GetDbCollection().InsertOne(dbContext.Context, newFoodstuff) // INSERT
+	if e2 != nil {
+		fmt.Println(fmt.Sprintf("Error from InsertOne(): %v", e2))
+		os.Exit(1);
+	} else {
+		fmt.Println(fmt.Sprintf("Successfully inserted foodstuff with ID %v, Result: %v", newFoodstuff.ID, result))
+	}
+}
+
+func GetFoodstuffEndpoint(response http.ResponseWriter, request *http.Request) {
+	dump(request)
+	fmt.Fprintf(os.Stdout, "Getting Foodstuff %s", request)
+}
 
 func main() {
-	fmt.Println("Starting the application...")
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	clientOptions := options.Client().ApplyURI("mongodb://food-db-server:27017")
-	client, _ = mongo.Connect(ctx, clientOptions)
+	fmt.Println("Starting the food-api...")
 	router := mux.NewRouter()
 	router.HandleFunc("/foodstuff", CreateFoodstuffEndpoint).Methods("POST")
-	router.HandleFunc("/recipes", GetRecipesEndpoint).Methods("GET")
 	router.HandleFunc("/foodstuff/{id}", GetFoodstuffEndpoint).Methods("GET")
-	http.ListenAndServe(":12345", router)
+	log.fatal(http.ListenAndServe(":12345", router))
 }
